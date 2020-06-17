5c3e7f069412a31e3c250a5a72204be7c45ae88c
Thomas Good <tom.good17@gmail.com>
2020-06-17 15:32:50 +1000

    Facebook Tool Project Push

---
+++ src/desktopfb/MainController.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desktopfb;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import static java.awt.Color.blue;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javax.swing.JTextArea;
+
+ 
+
+
+/**
+ * FXML Controller class
+ *
+ * @author thomasgood
+ */
+public class MainController implements Initializable {
+
+    
+    
+  
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+   
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Hello World!");
+        
+    }
+    
+    
+
+    
+    
+}
